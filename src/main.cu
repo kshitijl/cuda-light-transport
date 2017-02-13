0e26fe51d83ac0dfc63f7b42a3115113c4d6cc0c
lauria <kslauria@gmail.com>
2017-02-13 01:12:05 -0500

    Reorganize code

---
+++ src/main.cu
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+
+#include <iomanip>
+#include <ctime>
+#include <sstream>
+
+#include <string>
+
+#include <assert.h>
+
+#include "simple-interop.hxx"
+#include "write-ppm.hxx"
+
+#include "render.hxx"
+
+#include <GL/freeglut_ext.h>
+
+#include <json.hpp>
+
+using json = nlohmann::json;
+int main(int argc, char **argv) {
+  std::ifstream infile{"scene"};
+  std::string cc{std::istreambuf_iterator<char>(infile),
+      std::istreambuf_iterator<char>()};
+  auto j = json::parse(cc.c_str());
+
+  for(auto& element : j) {
+    if(element.count("emittance"))
+      std::cout << element["emittance"] << " ";
+    std::cout << element["center"] << "\n";
+  }
+
+  
+  return 0;
+}
